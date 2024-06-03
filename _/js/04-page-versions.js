263f5d0a1d868b4fa745232513960051b3b52337
Olivier Lamy <olamy@apache.org>
2024-06-03 15:47:25 +1000

    Site checkin for project Jetty :: Project :: Website

---
+++ _/js/04-page-versions.js
@@ -0,0 +1,1 @@
+!function(){"use strict";var t,e=document.querySelector(".page-versions .version-menu-toggle");e&&(t=document.querySelector(".page-versions"),e.addEventListener("click",function(e){t.classList.toggle("is-active"),e.stopPropagation()}),document.documentElement.addEventListener("click",function(){t.classList.remove("is-active")}))}();
